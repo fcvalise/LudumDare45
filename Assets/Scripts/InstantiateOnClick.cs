8d7e73adcf3cf698916fbc40fb1bed16e7fa7a52
Francois Corbel <francois.corbel@gmail.com>
2019-10-05 17:38:56 +0200

    Create shaders

---
+++ Assets/Scripts/InstantiateOnClick.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InstantiateOnClick : MonoBehaviour {
+	public GameObject _prefab;
+	void Update() {
+		if (Input.GetMouseButton(0)) {
+			Vector3 position = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+			position.z = 0;
+			GameObject go = Instantiate(_prefab, position, Quaternion.identity);
+		}
+	}
+}
